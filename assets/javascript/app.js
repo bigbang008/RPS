3edb3e5b6d59e9ad163d1b9094a0f050418ad092
SarawutM <imagination.mystery@gmail.com>
2018-05-08 17:28:34 -0500

    NEw Ver

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,215 +1,200 @@
-$(document).on("ready", function() {
+$(document).on('ready', function() {
 	// Links to Firebase
 	var database = new Firebase("https://addproject-787ce.firebaseio.com/");
 	var presenceRef = new Firebase('https://addproject-787ce.firebaseio.com/.info/connected');
 	var playersRef = new Firebase('https://addproject-787ce.firebaseio.com/playersRef');
 	var turnRef = new Firebase('https://addproject-787ce.firebaseio.com/turn');
-    var chatAppRef = new Firebase('https://addproject-787ce.firebaseio.com/chat');
-    
-    // Initialize variables
-	var playerNumberOne;
-    var playerNumberTwo;
+	var chatRef = new Firebase('https://addproject-787ce.firebaseio.com/chat');
+	// Initialize variables
+	var player;
+	var otherPlayer;
 	var name = {};
 	var userRef;
 	var wins1, wins2, losses1, losses2;
+	
 	var choices = ['rock','paper','scissors'];
 	
 	// Remove turn and chat when either player disconnects
 	turnRef.onDisconnect().remove();
-    chatAppRef.onDisconnect().remove();
-});
+	chatRef.onDisconnect().remove();
 
-	var setGame = {
-		addListener: function() {
-			// in case of the game is fulfiled with two people already
+	// Deal with android orientation change event
+	$(window).bind( 'orientationchange', function(e){
+		
+	});
+
+	// Game Object
+	var game = {
+		listeners: function() {
+			// Listen for a more than two clients
 			database.on("value", function(snapshot) {
 				var turnVal = snapshot.child('turn').val();
-				if (turnVal !== null && playerNumberOne == undefined) {
-                    var newDiv = $("<div>");
-                    newDiv.addClass("alert alert-warning");
-                    newDiv.attr("role", "alert");
-					var text = $("<h1>").text("Please wait, other players are playing -OR- come back again later :)");
-					newDiv.append(text);
-                    $(".hidden-msg").append(newDiv);
-                }
-            });
-            
-            //on click submit name
-			$(".name-add").on("click",function() {
-				setGame.setPlayer();
+				if (turnVal !== null && player == undefined) {
+					var wrapper = $('.wrapper');
+					var $img = $('<img>').attr('src',"assets/images/header.png");
+					var $h1 = $('<h1>').text('Please wait until other players finish, then refresh screen.');
+					wrapper.empty().append($img).append($h1);
+					throw new Error('Please wait until other players finish, then refresh screen.');
+				}
+			});
+			// Start button click
+			$('#addName').one('click',function() {
+				game.setPlayer();
 				return false;
-            });
-            
-			// Add player name
-			playersRef.on("child_added", function(childSnapshot) {
-
+			});
+			// Show player name in box
+			playersRef.on('child_added', function(childSnapshot) {
+				// Gets player number
 				var key = childSnapshot.key();
+				// Gets player names
 				name[key] = childSnapshot.val().name;
-                // Remove loading and add player name
-                var removeWaiting = $(key + ".waiting");
-                removeWaiting.empty();
-				var nameTag = $("<span>").text(name[key]);
-				$(".playername").append(nameTag);				
+				// Remove loading and add player name
+				var waiting = $('.player' + key + ' > .waiting');
+				waiting.empty();
+				var $h1 = $('<h1>').text(name[key]);
+				waiting.append($h1);				
 				// Get player wins and losses
 				var wins = childSnapshot.val().wins;
 				var losses = childSnapshot.val().losses;
-				var countWin = $("<p>").text("<i class='fas fa-trophy'></i>"+ "Wins: " + wins);
-				var countloss = $('<p>').text("<i class='fal fa-times-square'></i>"+ "Losses: " + losses);
-				$('.score' + key).append(countWin).append(countloss);
-            });
-            
-
+				var $wins = $('<h2>').text('Wins: ' + wins);
+				var $losses = $('<h2>').text('Losses: ' + losses);
+				$wins.addClass('float-left');
+				$losses.addClass('float-right');
+				$('.score' + key).append($wins).append($losses);
+			});
 			// Remove player name from box on disconnect
-			playersRef.on("child_removed", function(childSnapshot) {
+			playersRef.on('child_removed', function(childSnapshot) {
+				// Find player that was removed
 				var key = childSnapshot.key();
 				// Show 'player has disconnected' on chat
 				chat.sendDisconnect(key);
 				// Empty turn message
-				$("h4").text('Waiting for another player to join.');
+				$('h4').text('Waiting for another player to join.');
 				// Display beginning message
-                var waiting = $(key + ".waiting");
-                waiting.empty();
-                var waitingText = $("<h5>").text("Waiting for player " + key);
-                waitingText.addClass("card-title");
-                var loadingIcon = $("<p>")
-                loadingIcon.addClass("card-text");
-                var icon = $("<i>").addClass("fa fa-spinner fa-spin fa-one-large fa-fw");
-                loadingIcon.append(icon);
-                waiting.append(waitingText);
-                waiting.append(loadingIcon);
-				// clear score and choices
-				$(".score" + key).text('');
-				$(".player-one-choice").empty();
-				$(".show-result").empty();
-                $(".player-two-choice").empty();
-                var gameIcon = $("<p>").text("<i class='fas fa-gamepad'></i><i class='fas fa-gamepad'></i><i class='fas fa-gamepad'></i><i class='fas fa-gamepad'></i><i class='fas fa-gamepad'></i>")
-                $(".show-result").append(gameIcon);
-                $(".show-result").append(gameIcon);
-                $(".show-result").append(gameIcon);
-            });
-            
-            
-			// Turn function
+				var waiting = $('.player' + key + ' > .waiting');
+				waiting.empty();
+				var $h1 = $('<h1>').text('Waiting for player ' + key);
+				var $i = $('<i>').addClass('fa fa-spinner fa-spin fa-one-large fa-fw')
+				waiting.append($h1).append($i);
+				// Empty score
+				$('.score' + key).text('');
+				// Empty divs
+				$('.choices1').empty();
+				$('.results').empty();
+				$('.choices2').empty();
+			});
+			// Listen for each turn to direct to proper turn function
 			turnRef.on('value', function(snapshot) {
 				var turnNum = snapshot.val();
 				if (turnNum	== 1) {
 					// Empty divs
-					$(".player-one-choice").empty();
-					$(".show-reult").empty();
-                    $("..player-two-choice").empty();
-                    var gameIcon = $("<p>").text("<i class='fas fa-gamepad'></i><i class='fas fa-gamepad'></i><i class='fas fa-gamepad'></i><i class='fas fa-gamepad'></i><i class='fas fa-gamepad'></i>")
-                    $(".show-result").append(gameIcon);
-                    $(".show-result").append(gameIcon);
-                    $(".show-result").append(gameIcon);
-					setGame.turn1();
+					$('.choices1').empty();
+					$('.results').empty();
+					$('.choices2').empty();
+					game.turn1();
 				} else if (turnNum == 2) {
-					setGame.turn2();
+					game.turn2();
 				} else if (turnNum == 3){
-					setGame.turn3();
+					game.turn3();
 				}
-            });
-            
+			});
 			// Listen for change in wins and losses for players 1
-			playersRef.child(1).on("child_changed", function(childSnapshot) {
+			playersRef.child(1).on('child_changed', function(childSnapshot) {
 				if (childSnapshot.key() == 'wins') {
 					wins1 = childSnapshot.val();
-				} else if (childSnapshot.key() == "losses") {
+				} else if (childSnapshot.key() == 'losses') {
 					losses1 = childSnapshot.val();
 				}
 				// Update score display
 				if (wins1 !== undefined) {
-					$('.result-one .float-left').text('Wins: ' + wins1);
-					$('.result-one .float-right').text('Losses: ' + losses1);
+					$('.score1 .float-left').text('Wins: ' + wins1);
+					$('.score1 .float-right').text('Losses: ' + losses1);
 				}
 			});
 			// Listen for change in wins and losses for player 2
-			playersRef.child(2).on("child_changed", function(childSnapshot) {
-				if (childSnapshot.key() == "wins") {
+			playersRef.child(2).on('child_changed', function(childSnapshot) {
+				if (childSnapshot.key() == 'wins') {
 					wins2 = childSnapshot.val();
-				} else if (childSnapshot.key() == "losses") {
+				} else if (childSnapshot.key() == 'losses') {
 					losses2 = childSnapshot.val();
 				}
 				// Update score display
-				$('.result-two .float-left').text('Wins: ' + wins2);
-				$('.result-two .float-right').text('Losses: ' + losses2);
+				$('.score2 .float-left').text('Wins: ' + wins2);
+				$('.score2 .float-right').text('Losses: ' + losses2);
 			});
-        },
-        
+		},
 		setPlayer: function() {
-			
-			database.once("value", function(snapshot) {
-				var playerObj = snapshot.child("playersRef");
+			// Query database
+			database.once('value', function(snapshot) {
+				var playerObj = snapshot.child('playersRef');
 				var num = playerObj.numChildren();
 				// Add player 1
 			  if (num == 0) {
 					// Sets player to 1
 			  	player = 1;
-			  	setGame.addPlayer(player);
+			  	game.addPlayer(player);
 			  // Check if player 1 disconnected and re-add
 			  } else if (num == 1 && playerObj.val()[2] !== undefined) {
 					// Sets player to 1
 			  	player = 1;
-			  	setGame.addPlayer(player);
+			  	game.addPlayer(player);
 			  	// Start turn by setting turn to 1
 			  	turnRef.set(1);
 			  // Add player 2
 			  } else if (num == 1) {
 					// Sets player to 2
 			  	player = 2;
-			  	setGame.addPlayer(player);
+			  	game.addPlayer(player);
 			  	// Start turn by setting turn to 1
 					turnRef.set(1);
 			  }
 			});
-        },
-        
-        //grab name in the box
+		},
 		addPlayer: function(count) {
-			
-			var playerName = $('#inlineFormInputName').val();
+			// Gets player name
+			var playerName = $('#name-input').val();
 			// Remove form
-			var nameBox = $('.name-box');
-            nameBox.empty();
+			var greeting = $('.greeting');
+			greeting.empty();
 			// Show greeting
-			var greetingSentence = $("<h3>").text("Hello, " + playerName + '! You are Player ' + player);
-			nameBox.append(greetingSentence);
+			var $hi = $('<h3>').text('Hi ' + playerName + '! You are Player ' + player);
+			var $h4 = $('<h4>');
+			greeting.append($hi).append($h4);
 			// Create new child with player number
 			userRef = playersRef.child(count);
+			// Allows for disconnect
 			userRef.onDisconnect().remove();
+			// Sets children of player number
 			userRef.set({
-				"name": playerName,
-				"wins": 0,
-				"losses": 0
+				'name': playerName,
+				'wins': 0,
+				'losses': 0
 			});
-        },
-        
-		turnMessage: function(turn) {
-            playerNumberTwo = player == 1 ? 2:1;
-			if (turn == player) {
+		},
+		turnMessage: function(playTurn) {
+			otherPlayer = player == 1 ? 2:1;
+			if (playTurn == player) {
 				// Show its your turn
-				$("h4").text("It's Your Turn!");
-            } else if (turn == playerNumberTwo) {
+				$('h4').text("It's Your Turn!");
+			} else if (playTurn == otherPlayer) {
 				// Show waiting message
-                $("h4").text('Waiting for ' + name[playerNumberTwo] + ' to choose.');
+				$('h4').text('Waiting for ' + name[otherPlayer] + ' to choose.');
 			} else {
 				// Empty message for turn 3
-				$("h4").text('');
+				$('h4').text('');
 			}
-        },
-        
+		},
 		showChoice: function() {
 			for (i in choices) {
-				var iconChoices = $('<i>');
-				iconChoices.addClass('fa fa-hand-' + choices[i] + '-o fa-three-small');
-				iconChoices.attr('data-choice', choices[i]);
-				setGame.rotateChoice(player, iconChoices, choices[i]);
-				$('.choices' + player).append(iconChoices);
+				var $i = $('<i>');
+				$i.addClass('fa fa-hand-' + choices[i] + '-o fa-three-small');
+				$i.attr('data-choice', choices[i]);
+				game.rotateChoice(player, $i, choices[i]);
+				$('.choices' + player).append($i);
 			}
 			// Listen for choice
-			$(document).one('mousedown','i', setGame.setChoice);
-        },
-        
-
+			$(document).one('mousedown','i', game.setChoice);
+		},
 		setChoice: function() {
 			// Send selection to database
 			var selection = $(this).attr('data-choice');
@@ -217,12 +202,12 @@ $(document).on("ready", function() {
 				'choice': selection,
 			});
 			// Clear choices and add choice
-			var iconChoices = $('<i>');
-			iconChoices.addClass('fa fa-hand-' + selection + '-o fa-one-large');
-			iconChoices.attr('data-choice', selection);
-			iconChoices.addClass('position-absolute-choice' + player);
-			setGame.rotateChoice(player, iconChoices, selection);
-			$('.choices' + player).empty().append(iconChoices);
+			var $i = $('<i>');
+			$i.addClass('fa fa-hand-' + selection + '-o fa-one-large');
+			$i.attr('data-choice', selection);
+			$i.addClass('position-absolute-choice' + player);
+			game.rotateChoice(player, $i, selection);
+			$('.choices' + player).empty().append($i);
 			// Listen for turnNum
 			turnRef.once('value', function(snapshot) {
 				var turnNum = snapshot.val();
@@ -230,8 +215,7 @@ $(document).on("ready", function() {
 				turnNum++;
 				turnRef.set(turnNum);
 			});
-        },
-        
+		},
 		rotateChoice: function(person, element, choice) {
 			// Rotate each choice properly depending on the player
 			if (person == 1) {
@@ -249,28 +233,28 @@ $(document).on("ready", function() {
 		turn1: function() {
 			$('.player1').css('border','4px solid yellow');
 			// Show turn message
-			setGame.turnMessage(1);
+			game.turnMessage(1);
 			// Show choices to player 1
 			if (player == 1) {
-				setGame.showChoice();
+				game.showChoice();
 			}
 		},
 		turn2: function() {
 			$('.player1').css('border','1px solid black');
 			$('.player2').css('border','4px solid yellow');
 			// Show turn message
-			setGame.turnMessage(2);
+			game.turnMessage(2);
 			// Show choices to player 2
 			if (player == 2) {
-				setGame.showChoice();
+				game.showChoice();
 			}
 		},
 		turn3: function() {
 			$('.player2').css('border','1px solid black');
 			// Remove turn message
-			setGame.turnMessage(3);
+			game.turnMessage(3);
 			// Compute outcome
-			setGame.outcome();
+			game.outcome();
 		},
 		outcome: function() {
 			// Get choices, wins, and losses from database
@@ -284,41 +268,40 @@ $(document).on("ready", function() {
 				wins2 = snap2.wins;
 				losses2 = snap2.losses;
 				// Show other player's choice
-                var textChoice = playerNumberTwo == 1 ? choice1:choice2;
-				var iconChoices = $('<i>');
-				iconChoices.addClass('fa fa-hand-' + textChoice + '-o fa-one-large');
-                iconChoices.addClass('position-absolute-choice' + playerNumberTwo);
-				iconChoices.attr('data-choice', textChoice);
-                setGame.rotateChoice(playerNumberTwo, iconChoices, textChoice);
-                $('.choices' + playerNumberTwo).append(iconChoices);
+				var textChoice = otherPlayer == 1 ? choice1:choice2;
+				var $i = $('<i>');
+				$i.addClass('fa fa-hand-' + textChoice + '-o fa-one-large');
+				$i.addClass('position-absolute-choice' + otherPlayer);
+				$i.attr('data-choice', textChoice);
+				game.rotateChoice(otherPlayer, $i, textChoice);
+				$('.choices' + otherPlayer).append($i);
 
-				setGame.choiceAnimation();
+				game.choiceAnimation();
 			});
-        },
-        
+		},
 		logic: function() {
 			// Logic for finding winner
 			if (choice1 == choice2) {
-				setGame.winner(0);
+				game.winner(0);
 			} else if (choice1 == 'rock') {
 				if (choice2 == 'paper') {
-					setGame.winner(2);
+					game.winner(2);
 				} else if (choice2 == 'scissors') {
-					setGame.winner(1);
+					game.winner(1);
 				}
 			} else if (choice1 == 'paper') {
 				if (choice2 == 'rock') {
-					setGame.winner(1);
+					game.winner(1);
 				} else if (choice2 == 'scissors') {
-					setGame.winner(2);
+					game.winner(2);
 				}
 			} else if (choice1 == 'scissors') {
 				if (choice2 == 'rock') {
-					setGame.winner(2);
+					game.winner(2);
 				} else if (choice2 == 'paper') {
-					setGame.winner(1);
+					game.winner(1);
 				}
-            }
+			}
 		},
 		winner: function(playerNum) {
 			var results;
@@ -340,30 +323,29 @@ $(document).on("ready", function() {
 				wins++;
 				losses++;
 				// Gray loser
-                var playerNumberTwoNum = playerNum == 1 ? 2:1;
-                $('.choices' + playerNumberTwoNum + ' > i').css('opacity','0.5');
+				var otherPlayerNum = playerNum == 1 ? 2:1;
+				$('.choices' + otherPlayerNum + ' > i').css('opacity','0.5');
 				window.setTimeout(function() {
 					// Set the wins and losses
 					playersRef.child(playerNum).update({
 						'wins': wins
 					});
-                    playersRef.child(playerNumberTwoNum).update({
+					playersRef.child(otherPlayerNum).update({
 						'losses': losses
 					});
 				}, 500);
 			}
 			// Display results
 			window.setTimeout(function() {
-				$('.show-result').text(results);
+				$('.results').text(results).css('z-index','1');
 			}, 500);
 			// Change turn back to 1 after 3 seconds
 			window.setTimeout(function() {
 				// Reset turn to 1
 				turnRef.set(1);
-				$('.show-result').text('');
+				$('.results').text('').css('z-index','-1');
 			}, 2000);
-        },
-        
+		},
 		choiceAnimation: function() {
 		  var $choice1 = $('.choices1 > i');
 		  var $choice2 = $('.choices2 > i');
@@ -376,25 +358,24 @@ $(document).on("ready", function() {
 		  $choice2.addClass('animation-choice2');
 		  $choice2.one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function() {
 		    $choice2.addClass('choice2-end');
-		    setGame.logic();
+		    game.logic();
 		  });
 		}
 	}
-	// Start setGame
-	setGame.addListener();
+	// Start game
+	game.listeners();
 
 	// Chat object
 	var chat = {
 		message:'',
-		addListener: function() {
+		listeners: function() {
 			// Send button click
-			$('#submit-chat').on('click',function(event) {
+			$('#addMessage').on('click',function(event) {
 				chat.getMessage();
 				return false;
 			});
 			// Show message when received
-            chatAppRef
-    .on('child_added', function(childSnapshot) {
+			chatRef.on('child_added', function(childSnapshot) {
 				// Get name and message
 				var playerName = childSnapshot.val().name;
 				var message = childSnapshot.val().message;
@@ -416,15 +397,13 @@ $(document).on("ready", function() {
 			var obj = {};
 			obj['name'] = name[player];
 			obj['message'] = chat.message;
-            chatAppRef
-    .push(obj);
+			chatRef.push(obj);
 		},
 		sendDisconnect: function(key) {
 			var obj = {};
 			obj['name'] = name[key];
 			obj['message'] = ' has disconnected.';
-            chatAppRef
-    .push(obj);
+			chatRef.push(obj);
 		},
 		showMessage: function(playerName, message) {
 			// Auto scroll to bottom variables
@@ -434,16 +413,16 @@ $(document).on("ready", function() {
 			var $p = $('<p>');
 			if (message == ' has disconnected.' && player !== undefined) {
 				$p.text(playerName + message);
-				$p.css('background','gray');
+				$p.css('background','red');
 			} else if (player !== undefined) {
 				$p.text(playerName + ': ' + message);
 			}
-			// If player 1 -> blue text
+			// If player 1 -> black text
 			if (name[1] == playerName) {
-				$p.css('color','blue');
-			// If player 2 -> red text
+				$p.css('color','black');
+			// If player 2 -> white text
 			} else if (name[2] == playerName) {
-				$p.css('color','red');
+				$p.css('color','white');
 			}
 			// Append message
 			if ($p.text() !== '') {
@@ -457,5 +436,5 @@ $(document).on("ready", function() {
 	}
 
 	// Start chat
-	chat.addListener();
+	chat.listeners();
 });
